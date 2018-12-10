e0d3d3e2644d4da8a76d1b59bcae143a85b5b9f2
blopp1 <44609671+blopp1@users.noreply.github.com>
2018-12-10 16:50:36 +0100

    Initial commit

---
+++ MaterialHider/Scripts/MaterialHider.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XOAProductions.Utility
+{
+    /// <summary>
+    /// a quick little script that can turn entire hierarchies of renderers invisible/visible
+    /// </summary>
+    public class MaterialHider
+    {
+
+        /// <summary>
+        /// Turns the hierarchy of renderers invisible
+        /// </summary>
+        /// <param name="go">the gameobject who's own renderer and all child renderers should be hidden</param>
+        /// <param name="id">assign a id to the parts that will be hidden, so you can later turn only those renderers back on that you want</param>
+       public void HideHierarchy(GameObject go, string id)
+        {
+           Renderer[] renderers =  go.GetComponentsInChildren<Renderer>();
+           
+           foreach(Renderer r in renderers)
+            {
+                MaterialHiderBackup backup = r.gameObject.AddComponent<MaterialHiderBackup>();
+                backup.id = id;
+                backup.savedMaterials = new List<Material>(r.materials);
+                Material[] mats = r.materials;
+
+                for(int i = 0; i < r.materials.Length; i++)
+                {
+                    mats[i] = getInvisibleMaterial();
+                }
+
+                r.materials = mats;
+            }
+
+        }
+
+       /// <summary>
+        /// Makes a previously hidden hierachry visible again
+        /// </summary>
+        /// <param name="go">the gameobject who's childrenderers and own renderer has been hidden previously</param>
+        /// <param name="id">the id that you specified when calling hidehierarchy</param>
+       public void DisplayHierarchy(GameObject go, string id)
+        {
+            MaterialHiderBackup[] backups = go.GetComponentsInChildren<MaterialHiderBackup>();
+
+            foreach(MaterialHiderBackup b in backups)
+            {
+                if (b.id != id)
+                    continue;
+
+                var renderer = b.GetComponent<Renderer>();
+                renderer.materials = b.savedMaterials.ToArray();
+
+                
+            }
+
+            foreach (MaterialHiderBackup b in backups)
+            {
+                if (b.id != id)
+                    continue;
+
+                Object.Destroy(b);
+            }
+        }
+
+        /// <summary>
+        /// the material that's used to make renderers invisible
+        /// </summary>
+        /// <returns>an invisible material from Resources/Materials/Invisible.mat</returns>
+        public Material getInvisibleMaterial()
+        {
+            return Resources.Load<Material>("Materials/Invisible");
+        }
+ 
+    }
+}
---
+++ MaterialHider/Scripts/MaterialHiderBackup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XOAProductions.Utility
+{
+    /// <summary>
+    /// keeps the original materials of the renderer as a backup 
+    /// so we can later turn the renderer visible again without issues.
+    /// </summary>
+    public class MaterialHiderBackup : MonoBehaviour
+    {
+        //backup for the previous materials 
+        public List<Material> savedMaterials = new List<Material>();
+
+        //id so we can mark groups to toggle them
+        //helps if you add 2 hierarchies of renderers that have been turned invisible
+        //and you just want to turn one of them back on
+        public string id;
+    }
+}
+ 
